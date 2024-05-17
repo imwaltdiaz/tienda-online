6deded466fa623876582de574687cbf217ef61e4
Your Name <you@example.com>
2024-05-17 03:05:55 -0500

    carrito de compras

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/pages/admin_paginaPrincipal';
-import ItemsCarro from './components/pages/carritoDeCompras';
 import Header from './components/common/header';
 import Footer from './components/common/footer';
 import ItemsCheckOut, { BoxDireccion , MetodoEnvio, BoxPago, Prueba, BoxItem, BoxResOrd} from './components/pages/checkout';
+import ItemsCarro, {ListaItemCarr, Compil, BtnPago} from './components/pages/carritoDeCompras';
 import { Box } from '@mui/material';
+import { CoPresent } from '@mui/icons-material';
 
 
 
@@ -13,16 +14,9 @@ function App() {
   return (
     <>
       <Header/>
-      <ItemsCheckOut/>
-      <Box display={'flex'} justifyContent="space-evenly">
-        <BoxDireccion/>
-        <BoxPago/>
-      </Box>
-      <MetodoEnvio/>
-      <Box display={'flex'} justifyContent= "space-evenly">
-        <BoxItem/>
-        <BoxResOrd/>
-      </Box>
+      <Compil/>
+      <BtnPago/>
+      <Compil/>
       <Footer/>
     </>
   );
--- src/components/pages/carritoDeCompras.js
+++ src/components/pages/carritoDeCompras.js
@@ -1,19 +1,57 @@
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { grey } from '@mui/material/colors';
-
-
-
+import AspectRatio from '@mui/joy/AspectRatio';
+import Link from '@mui/joy/Link';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Divider from '@mui/material/Divider';
+import { Stack } from '@mui/material';
+import ListaItemCarr from '../common/ObjCarrito';
 
 export default function ItemsCarro() {
   return (
-    <><><Typography variant="h4" component="p">
-          2. Items en tu Carrito de Compras
-      </Typography></><Box component="section" sx={{ p: 2, border: '1px solid black', background :'#C2C1C1'}}>
-                Items Disponible para Envio
-          </Box></>
+    <>
+      <Typography variant="h4" component="p">
+        Items en tu Carrito de Compras
+      </Typography>
+      <Box component="section" sx={{ p: 2, border: '1px solid black', background: '#C2C1C1' }}>
+        Items Disponible para Envio
+      </Box>
+    </>
   );
 }
+
+export function Compil() {
+  return (
+    <>
+      <ListaItemCarr
+        imagen="https://blog.bangbranding.com/wp-content/uploads/2016/11/700x511_SliderInterior.jpg"
+        nombre="nombre1"
+        cant="cant"
+        prize="prize"
+        subtot="subtot"
+      />
+    </>
+  );
+}
+export function BtnPago() {
+  return (
+    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', marginRight: '50px', marginBottom: '20px' }}>
+  <Typography variant="h5" component="p" alignItems={"flex-end"}>
+    Total: S/ 100
+  </Typography>
+  <button>Checkout</button>
+</Box>
+<Box component="section" sx={{ p: 1, border: '1px solid black', background: '#C2C1C1', marginTop: '20px' }}>
+  Datos de compra
+</Box>
+
+      </>
+  );
+}
+
+
 
 

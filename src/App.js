f88f8574a68a81ebdc9269ea86811437bb287f8b
HollowedRino <20200865@aloe.ulima.edu.pe>
2024-05-20 23:19:34 -0500

    registro Usuarios completo

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Collections from './components/Main/mainPage';
 import Details from './components/Main/detailsPage';
 import Busqueda from './components/Main/busquedaP';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Datos_Usuario } from './components/pages/datos_Usuario';
 
 
 function App() {
@@ -18,11 +19,11 @@ function App() {
           <Route index element={<Collections/>}/>
           <Route path="/busqueda" element={<Busqueda/>}/>
           <Route path="/detalles" element={<Details/>}/>
-          
+          <Route path="/datos_usuario" element={<Datos_Usuario/>}/>
         </Routes>
-      
-      </BrowserRouter>        
-    <AdminMain></AdminMain>
+      </BrowserRouter>      
+
+      <AdminMain></AdminMain>
     </>
   );
 }
